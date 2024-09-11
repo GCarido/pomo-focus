1055fb311481f4afc8c4a2e4a74478fea4e5263e
Gerald Carido <112929169+GCarido@users.noreply.github.com>
2024-09-11 20:32:39 +0800

    Make div clickable for toggle theme

--- components/toggle/ThemeToggler.tsx
+++ components/toggle/ThemeToggler.tsx
@@ -15,13 +15,13 @@ const ThemeToggler = () => {
     return (
         <div>
             {/* slider */}
-            <div className="shadow-lg outline-double outline-2 outline-primary w-[80px] rounded-md dark:outline-primary">
+            <div 
+            onClick={toggleTheme}
+            className="shadow-lg outline-double outline-2 outline-primary w-[80px] rounded-md dark:outline-primary">
                 <Button
                     variant="outline"
                     size="icon"
-                    onClick={toggleTheme}
-                    className="dark:translate-x-11 transition-all"
-                >
+                    className="dark:translate-x-11 transition-all">
                     <SunIcon className="h-5 w-5 rotate-0 dark:scale-0 dark:rotate-90 scale-100 transition-all" />
                     <MoonIcon className="h-5 w-5 absolute scale-0 rotate-90 dark:scale-100 dark:rotate-0 transition-all" />
                 </Button>
