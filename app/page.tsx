8cdf69a295d662c8dfe9d932c089900935e0fc86
Gerald Carido <112929169+GCarido@users.noreply.github.com>
2024-09-12 23:14:49 +0800

    Remove placeholder and add card content

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,13 @@
-import ThemeToggler from '@/components/toggle/ThemeToggler'
+import Card from '@/components/PomoCard'
 
 const Home = () => {
   return (
     <main className="px-[10%] xl:px-[20%]">
       <div className="container mx-auto">
         {/* card component here */}
-        <h1 className="h1">Header 1</h1>
-        <h2 className="h2">Header 2</h2>
-        <h3 className="h3">Header 3</h3>
-        <h4 className="h4">Header 4</h4>
-        <p className="subtitle">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis odit nobis aperiam quo velit iusto obcaecati nostrum alias? Consectetur architecto sapiente debitis rerum corporis veniam harum quidem. Dolore, expedita quis!</p>
-        <ThemeToggler />
+        <div className="flex justify-center">
+          <Card />
+        </div>
       </div>
     </main>
   )
