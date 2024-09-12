80d15324f40276150af8de226063432e3b174543
Gerald Carido <112929169+GCarido@users.noreply.github.com>
2024-09-12 18:44:26 +0800

    Add transition to accessing pages

---
+++ app/template.tsx
@@ -0,0 +1,24 @@
+"use client"
+import React from 'react'
+import { motion } from 'framer-motion'
+
+const variants = {
+    hidden: { opacity: 0 },
+    enter: { opacity: 1 },
+};
+
+const Template = ({ children }: any) => {
+    return (
+        <>
+            <motion.main
+                variants={variants}
+                initial="hidden"
+                animate="enter"
+                transition={{ type: "linear", delay: 0.2, duration: 0.5 }}>
+                {children}
+            </motion.main>
+        </>
+    )
+}
+
+export default Template
