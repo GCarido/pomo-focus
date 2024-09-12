183ad3fc549dc286008043f0c6a814dbf9d793b7
Gerald Carido <112929169+GCarido@users.noreply.github.com>
2024-09-12 23:15:24 +0800

    Add pomo card content and styling

---
+++ components/PomoCard.tsx
@@ -0,0 +1,96 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardFooter,
+  CardTitle
+} from "@/components/ui/card";
+
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger
+} from "@/components/ui/tabs";
+
+import { useTheme } from "next-themes";
+import ThemeToggler from "./toggle/ThemeToggler";
+import { i } from "framer-motion/client";
+
+const PomoCard = () => {
+
+  //Check if current theme is "light"
+  const { theme } = useTheme();
+  const isLightMode = theme === "light";
+  const [bgColor, setBgColor] = useState("190 100% 95%"); // Default color
+
+  const handleTabChange = (value: string) => {
+    let newBgColor;
+
+    switch (value) {
+      case 'short-break':
+        newBgColor = "357, 100%, 92%"; // Red
+        break;
+      case 'long-break':
+        newBgColor = "187 100% 78%"; // Blue
+        break;
+      default:
+        newBgColor = "190 100% 95%"; //Default
+    }
+
+    if (isLightMode) {
+      document.documentElement.style.setProperty('--background', newBgColor);
+    }
+
+    setBgColor(newBgColor);
+  };
+
+  useEffect(() => {
+    if(!isLightMode) {
+      document.documentElement.style.removeProperty("--background");
+    } else {
+      document.documentElement.style.setProperty("--background", bgColor);
+    }
+
+    return () => {
+      document.documentElement.style.removeProperty("--background");
+    }
+  }, [isLightMode, bgColor]);
+
+  return (
+    <Card className="w-[500px]">
+      {/* toggle theme */}
+      <div className="flex justify-end p-1">
+        <ThemeToggler />
+      </div>
+      <CardHeader className="items-center">
+        <p className="h2 md:text-5xl">Get Productive!</p>
+      </CardHeader>
+      <CardContent className="flex justify-center">
+        <Tabs defaultValue="pomodoro" onValueChange={handleTabChange}>
+          <TabsList>
+            <TabsTrigger value="pomodoro">Pomodoro</TabsTrigger>
+            <TabsTrigger value="short-break">Short Break</TabsTrigger>
+            <TabsTrigger value="long-break">Long Break</TabsTrigger>
+          </TabsList>
+          <TabsContent value="pomodoro">
+            Pomodoro
+          </TabsContent>
+          <TabsContent value="short-break">
+            Short Break
+          </TabsContent>
+          <TabsContent value="long-break">
+            Long Break
+          </TabsContent>
+        </Tabs>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default PomoCard;
