eb81f6abf090bdf049e46f6e9ab561f2ee395a59
Gerald Carido <112929169+GCarido@users.noreply.github.com>
2024-09-13 15:14:42 +0800

    Add timer component

---
+++ components/Timer.tsx
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from 'react'
+import { Button } from './ui/button';
+
+interface TimerProps {
+    tabName?: string;
+};
+
+const Timer = ({ tabName = "" }: TimerProps) => {
+    const [isActive, setIsActive] = useState(false);
+    const [isComplete, setIsComplete] = useState(false);
+
+    const defaultSeconds = {
+        'short break': 300,
+        'long break': 900
+    }[tabName] || 1500;
+
+    const [seconds, setSeconds] = useState(defaultSeconds);
+
+    useEffect(() => {
+        let interval: any;
+
+        if (isActive) {
+            setIsComplete(false);
+            interval = setInterval(() => {
+                setSeconds((prevSeconds) => prevSeconds - 1);
+            }, 1000);
+        }
+        if (seconds === 0) {
+            setIsActive(false);
+            setSeconds(defaultSeconds);
+            setIsComplete(true);
+        }
+        return () => clearInterval(interval);
+    }, [isActive, seconds]);
+
+    const reset = () => {
+        setSeconds(defaultSeconds);
+        setIsActive(false);
+    };
+
+    return (
+        <div className="p-6 flex flex-col items-center h-[200px]">
+            {/* time display */}
+            <p className="h1">{('0' + Math.floor(seconds / 60)).slice(-2)}:{('0' + (seconds % 60)).slice(-2)}</p>
+            {/* button and task info display */}
+            <div className="pt-2 flex flex-col items-center gap-y-4">
+                {/* info */}
+                <p className="text-foreground capitalize dark:text-green-300 font-bold">{`${isComplete ? `${tabName} completed!` : ""}`}</p>
+                {/* button container */}
+                <div className="flex gap-x-4">
+                    <Button
+                        className="w-[100px]"
+                        variant="outline"
+                        onClick={() => setIsActive(!isActive)}>
+                        {isActive ? "Pause" : "Start"}
+                    </Button>
+                    <Button
+                        className="w-[100px]"
+                        variant="outline"
+                        onClick={reset}>
+                        Reset
+                    </Button>
+                </div>
+            </div>
+
+
+        </div>
+    )
+}
+
+export default Timer
