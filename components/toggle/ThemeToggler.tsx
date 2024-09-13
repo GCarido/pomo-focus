86e29a3d9dd14e35b8c1357b4f485eb7ce494e9e
Gerald Carido <112929169+GCarido@users.noreply.github.com>
2024-09-13 15:51:44 +0800

    Add theme sound effect

--- components/toggle/ThemeToggler.tsx
+++ components/toggle/ThemeToggler.tsx
@@ -1,15 +1,26 @@
 "use client"
 
-import { Button } from "../ui/button"
 import { SunIcon, MoonIcon } from "@radix-ui/react-icons"
 import { useTheme } from 'next-themes'
+
+import { Button } from "../ui/button"
+
+import useSound from "use-sound"
+
 
 const ThemeToggler = () => {
     const { theme, setTheme } = useTheme();
     const isDarkMode = theme === "dark";
-
+    const [playLightSwitch] = useSound("../sounds/light-mode.mp3");
+    const [playDarkSwitch] = useSound("../sounds/dark-mode.mp3");
     const toggleTheme = () => {
-        setTheme(isDarkMode ? "light" : "dark");
+        if(isDarkMode) {
+            playLightSwitch();
+            setTheme("light");
+        } else {
+            playDarkSwitch();
+            setTheme("dark");
+        }
     };
 
     return (
@@ -22,6 +33,7 @@ const ThemeToggler = () => {
                     variant="outline"
                     size="icon"
                     className="dark:translate-x-11 transition-all">
+
                     <SunIcon className="h-5 w-5 rotate-0 dark:scale-0 dark:rotate-90 scale-100 transition-all" />
                     <MoonIcon className="h-5 w-5 absolute scale-0 rotate-90 dark:scale-100 dark:rotate-0 transition-all" />
                 </Button>
