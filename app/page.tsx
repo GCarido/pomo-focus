f64c047feefd364ee1991ffdb7d2498f40b5e480
Gerald Carido <112929169+GCarido@users.noreply.github.com>
2024-09-14 22:56:08 +0800

    Add tasks to the main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Card from '@/components/PomoCard'
+import Tasks from '@/components/Tasks'
 
 const Home = () => {
   return (
     <main className="px-[10%] xl:px-[20%]">
       <div className="container mx-auto">
         {/* card component here */}
-        <div className="flex justify-center">
+        <div className="flex items-center flex-col">
           <Card />
+          <Tasks />
         </div>
       </div>
     </main>
