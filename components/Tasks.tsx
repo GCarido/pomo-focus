474fb71bc5fdb062d4b5aba20032024ce73f3869
Gerald Carido <112929169+GCarido@users.noreply.github.com>
2024-09-14 22:55:48 +0800

    Initial setup for tasks component

---
+++ components/Tasks.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Tasks = () => {
+  return (
+    <section className="pt-8">
+        <div className="container mx-auto">
+            <div className="border-b-4 border-accent-foreground w-[500px] flex justify-center">
+                <h3 className="h3">Tasks</h3>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Tasks
