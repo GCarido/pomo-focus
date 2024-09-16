919ffaf2a0a19b5f95871cf390c69efb06f9b466
Gerald Carido <112929169+GCarido@users.noreply.github.com>
2024-09-16 16:52:09 +0800

    Add edit task content and logic

---
+++ components/EditTask.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import { Input } from './ui/input'
+import { Button } from './ui/button'
+
+interface EditTaskProps {
+    itemValue: string;
+};
+
+const EditTask = ({ itemValue }: EditTaskProps) => {
+    const [inputValue, setInputValue] = useState(itemValue);
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+    }
+
+    const handleEdit = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInputValue(e.target.value);
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div className="flex flex-col gap-y-4">
+                <Input
+                    type="text"
+                    className="w-full"
+                    value={inputValue}
+                    onChange={handleEdit}
+                />
+                <Button
+                    variant="outline"
+                    type="submit"
+                    className="w-full"
+                >
+                    Save
+                </Button>
+            </div>
+
+        </form>
+    )
+}
+
+export default EditTask
