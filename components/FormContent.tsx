1b674b13bae00ac7ca172bddf5f3e2b80be22976
Gerald Carido <112929169+GCarido@users.noreply.github.com>
2024-09-16 16:52:25 +0800

    Add editing condition

--- components/FormContent.tsx
+++ components/FormContent.tsx
@@ -1,4 +1,7 @@
 import { CheckCircle, Edit, Trash2 } from "lucide-react"
+
+import EditTask from "./EditTask";
+
 import { useState } from "react";
 
 interface Task {
@@ -6,15 +9,17 @@ interface Task {
     taskName: string;
     completed: boolean;
     isEditing: boolean;
-}
+};
 
 interface FormContentProps {
     tasks: Task[];
     onDelete: (id: string) => void;
     onComplete: (id: string) => void;
+    onEdit: (id: string) => void;
 };
 
-const FormContent = ({ tasks, onDelete, onComplete }: FormContentProps) => {
+const FormContent = ({ tasks, onDelete, onComplete, onEdit }: FormContentProps) => {
+    const [newTask, setNewTask] = useState("");
 
     const handleDelete = (id: string) => {
         onDelete(id);
@@ -24,33 +29,50 @@ const FormContent = ({ tasks, onDelete, onComplete }: FormContentProps) => {
         onComplete(id);
     };
 
+    const handleEdit = (id: string) => {
+        onEdit(id);
+    };
+
     return (
         <div className="cursor-pointer flex flex-col gap-y-5">
             {tasks.map(task => (
                 <div key={task.id}>
-                    <div className="border-2 rounded-sm border-accent-foreground/70 p-5 flex items-center justify-between">
-                        {/* check and text */}
-                        <div className="flex gap-x-5 items-center">
-                            <CheckCircle
-                                size={30}
-                                className={`hover:dark:text-blue-500 hover:text-blue-400 ${task.completed ? "text-primary dark:text-primary" : ""}`}
-                                onClick={() => handleComplete(task.id)}
-                            />
-                            <p className={`h5 hover:dark:text-blue-300 hover:text-blue-400 ${task.completed ? "line-through text-primary dark:text-primary" : ""}`}>{task.taskName}</p>
+
+                    {/* test */}
+                    {task.isEditing ? "true" : "false"} {task.taskName}
+
+                    {/* test editing condition */}
+                    {task.isEditing ? (
+                        <EditTask 
+                            itemValue={task.taskName}
+                        />
+                    ) : (
+                        <div className="border-2 rounded-sm border-accent-foreground/70 p-5 flex items-center justify-between">
+                            {/* check and text */}
+                            <div className="flex gap-x-5 items-center">
+                                <CheckCircle
+                                    size={30}
+                                    className={`hover:dark:text-blue-500 hover:text-blue-400 ${task.completed ? "text-primary dark:text-primary" : ""}`}
+                                    onClick={() => handleComplete(task.id)}
+                                />
+                                <p className={`hover:dark:text-blue-300 hover:text-blue-400 ${task.completed ? "line-through text-primary dark:text-primary" : ""}`}>{task.taskName}</p>
+                            </div>
+                            <div className="flex gap-x-2">
+                                {/* edit and delete */}
+                                <Edit
+                                    size={30}
+                                    className="hover:dark:text-blue-500 hover:text-blue-400"
+                                    onClick={() => handleEdit(task.id)}
+                                />
+                                <Trash2
+                                    size={30}
+                                    className="hover:dark:text-blue-500 hover:text-blue-400"
+                                    onClick={() => handleDelete(task.id)}
+                                />
+                            </div>
                         </div>
-                        <div className="flex gap-x-2">
-                            {/* edit and delete */}
-                            <Edit
-                                size={30}
-                                className="hover:dark:text-blue-500 hover:text-blue-400"
-                            />
-                            <Trash2
-                                size={30}
-                                className="hover:dark:text-blue-500 hover:text-blue-400"
-                                onClick={() => handleDelete(task.id)}
-                            />
-                        </div>
-                    </div>
+                    )}
+
                 </div>
             ))}
 
