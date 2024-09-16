3efa408ffea374741be72edef75a2cac9e16d220
Gerald Carido <112929169+GCarido@users.noreply.github.com>
2024-09-16 14:27:04 +0800

    Add FormTask content styling and logic

---
+++ components/FormTasks.tsx
@@ -0,0 +1,91 @@
+import { useState } from "react"
+
+import { Button } from "./ui/button"
+import { Input } from "./ui/input"
+
+import FormContent from "./FormContent"
+
+import { v4 as uuidv4 } from "uuid"
+
+interface Task {
+    id: string;
+    taskName: string;
+    completed: boolean;
+    isEditing: boolean;
+}
+
+const FormTasks = () => {
+    const [inputValue, setInputValue] = useState("");
+    const [addTask, setAddTask] = useState<Task[]>([]);
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+        setInputValue(e.target.value);
+    }
+
+    const handleTask = (taskName: string): void => {
+        setAddTask([
+            ...addTask,
+            {
+                id: uuidv4(),
+                taskName,
+                completed: false,
+                isEditing: false
+            }
+        ]);
+    }
+    
+    const handleButtonClick = () => {
+        if (inputValue.trim() !== "") {
+            handleTask(inputValue);
+            setInputValue("");
+        }
+    }
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+        e.preventDefault();
+    }
+
+    const handleDelete = (id: string): void => {
+        setAddTask(addTask.filter(task => task.id !== id));
+    }
+
+    const handleComplete = (id: string): void => {
+        setAddTask(addTask.map(task => 
+            task.id === id ? { ...task, completed: !task.completed } : task
+        ));
+    }
+
+    
+    return (
+        <div className="flex flex-col gap-y-8">
+            {/* form input */}
+            <form className="pt-6" onSubmit={handleSubmit}>
+                {/* input and button */}
+                <div className="flex gap-x-4">
+                    <Input
+                        type="text"
+                        placeholder="What's on your plate?"
+                        value={inputValue}
+                        onChange={handleInputChange}
+                    />
+                    <Button
+                        variant="outline"
+                        onClick={handleButtonClick}
+                    >
+                        Add Task
+                    </Button>
+                </div>
+            </form>
+            
+            {/* form content */}
+            <FormContent 
+            tasks={addTask.sort((a, b) => Number(a.completed) - Number(b.completed))} 
+            onDelete={handleDelete} 
+            onComplete={handleComplete}
+            />
+
+        </div>
+    )
+}
+
+export default FormTasks
