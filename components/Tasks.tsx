a20431cb6983e8124c197e061b204d40aa5764a9
Gerald Carido <112929169+GCarido@users.noreply.github.com>
2024-09-16 14:28:27 +0800

    Add responsive width and client side rendering

--- components/Tasks.tsx
+++ components/Tasks.tsx
@@ -1,13 +1,21 @@
+"use client"
+
 import React from 'react'
+import FormTasks from './FormTasks'
 
 const Tasks = () => {
   return (
-    <section className="pt-8">
-        <div className="container mx-auto">
-            <div className="border-b-4 border-accent-foreground w-[500px] flex justify-center">
-                <h3 className="h3">Tasks</h3>
-            </div>
+    <section className="pt-8 w-[300px] md:w-[500px]">
+      <div className="container mx-auto">
+        {/* task header */}
+        <div className="border-b-4 border-accent-foreground flex justify-center">
+          <h3 className="h3">Tasks</h3>
         </div>
+        {/* form task component */}
+        <div>
+          <FormTasks />
+        </div>
+      </div>
     </section>
   )
 }
