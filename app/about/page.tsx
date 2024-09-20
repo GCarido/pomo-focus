0ac5358c1a43d6fa5bcd9224798b0dbbc4476b6e
Gerald Carido <112929169+GCarido@users.noreply.github.com>
2024-09-21 00:06:13 +0800

    Add paragraph styling placeholder

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,12 +2,13 @@ import React from 'react'
 
 const About = () => {
   return (
-    <section className="px-[10%] xl:px-[20%]">
+    <section className="">
       <div className="container mx-auto">
         <div>
-          <h1 className="h1">
-              About Page
-          </h1>
+          <h2 className="h2">
+              What is Pomodoro?
+          </h2>
+          <p className="subtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam ex sint expedita corporis iure ratione reiciendis necessitatibus debitis culpa totam doloribus, doloremque, placeat at, tempora iusto perspiciatis sit. Iste, nemo?</p>
         </div>
       </div>
     </section>
