ccb5bb7f3ee4b95318ed455ecb4647b94f39f871
Gerald Carido <112929169+GCarido@users.noreply.github.com>
2024-09-24 16:17:17 +0800

    Add speed insights component in layout page

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,9 +6,11 @@ import { ThemeProvider } from "@/components/provider/ThemeProvider";
 
 import Header from "@/components/Header";
 
+import { SpeedInsights } from "@vercel/speed-insights/react";
+
 export const metadata: Metadata = {
   title: "Pomo focus",
-  description: "A simple pomodoro timer app with todo list", 
+  description: "A simple pomodoro timer app with todo list",
 };
 
 export default function RootLayout({
@@ -18,12 +20,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className="antialiased font-suse">
-        <ThemeProvider attribute="class" defaultTheme="light">
-          <Header />
-          {children}
-        </ThemeProvider>
+      <body className="antialiased font-suse">
+          <ThemeProvider attribute="class" defaultTheme="light">
+            <Header />
+            {children}
+            <SpeedInsights />
+          </ThemeProvider>
       </body>
     </html>
   );
